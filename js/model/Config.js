0531caa049399550d4654db1a333bbf1e18eb403
Luca Tomasi <lucatomasi77@gmail.com>
2024-12-04 14:41:49 +0100

    Fix: default sound config set to true

--- js/model/Config.js
+++ js/model/Config.js
@@ -1,6 +1,6 @@
 class Config extends ORM {
 
-    constructor(id = null, video=false, sound=false) {
+    constructor(id = null, video=false, sound=true) {
         super(id, {'video': video, 'sound':sound});
     }
 
